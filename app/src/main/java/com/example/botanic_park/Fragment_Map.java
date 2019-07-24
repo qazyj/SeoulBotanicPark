2135b8a04ea6b4ede75cf938fb3f81f25c0a9162
Sawon <sawon49@naver.com>
2019-07-24 21:57:47 +0900

    네이버 API

--- app/src/main/java/com/example/botanic_park/Fragment_Map.java
+++ app/src/main/java/com/example/botanic_park/Fragment_Map.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.naver.maps.map.MapView;
 
 public class Fragment_Map extends Fragment {
+
+    private MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,5 +18,55 @@ public class Fragment_Map extends Fragment {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view,
+                              Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
