c00a7c3963572384635f8a36f4bd41b29acb97c1
jin-ryu <ann0905@naver.com>
2019-07-29 00:11:08 +0900

    식물 리스트 크롤링 해온거 저장 (저장된거 있으면 크롤링 안해옴)

--- app/src/main/java/com/example/botanic_park/MainActivity.java
+++ app/src/main/java/com/example/botanic_park/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.botanic_park;
 import android.app.Activity;
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -12,6 +13,7 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
@@ -27,7 +29,11 @@ import android.view.View;
 import android.widget.Toast;
 import com.example.botanic_park.PlantSearch.Fragment_Plant_Book;
 import com.example.botanic_park.PlantSearch.PlantBookItem;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import java.security.MessageDigest;
@@ -43,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private Fragment_Plant_Book fragment_Plant_Book;
     private Fragment_Information fragment_Information;
 
-    private ArrayList<PlantBookItem> list;
+    private ArrayList<PlantBookItem> list = null;
 
     private CurveBottomBar curveBottomBar;
 
@@ -54,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 로딩 액티비티에서 파싱해온 정보 받음
-        final Intent intent = getIntent();
+        // 로딩 액티비티에서 식물 리스트 정보 받아옴
+        Intent intent = getIntent();
         list = (ArrayList<PlantBookItem>) intent.getSerializableExtra(LoadingActivity.PLANT_LIST_KEY);
 
         //상태 바 색 바꿔줌
@@ -127,16 +133,16 @@ public class MainActivity extends AppCompatActivity {
             switch(menuItem.getItemId())
             {
                 case R.id.home:
-                    transaction.replace(R.id.frame_container, fragment_Home).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_container, fragment_Home).commit();
                     break;
                 case R.id.map:
-                    transaction.replace(R.id.frame_container, fragment_Map).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_container, fragment_Map).commit();
                     break;
                 case R.id.plant_book:
-                    transaction.replace(R.id.frame_container, fragment_Plant_Book).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_container, fragment_Plant_Book).commit();
                     break;
                 case R.id.information:
-                    transaction.replace(R.id.frame_container, fragment_Information).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_container, fragment_Information).commit();
                     break;
             }
             return true;
