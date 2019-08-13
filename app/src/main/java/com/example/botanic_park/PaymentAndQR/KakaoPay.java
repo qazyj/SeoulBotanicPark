b734063631f56f12ce73a44f5acd8b9578c1ffbd
Sawon <sawon49@naver.com>
2019-08-14 02:15:16 +0900

    결제

--- app/src/main/java/com/example/botanic_park/PaymentAndQR/KakaoPay.java
+++ app/src/main/java/com/example/botanic_park/PaymentAndQR/KakaoPay.java
@@ -14,7 +14,6 @@ import kr.co.bootpay.enums.PG;
 public class KakaoPay extends Activity {
 
     private String application_id = "5d43308c02f57e0037f7fdaa";
-    private KakaoPay kakaoPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +21,6 @@ public class KakaoPay extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kakao_pay);
         BootpayAnalytics.init(this, application_id);
-        kakaoPay = this;
         // 초기설정 - 해당 프로젝트(안드로이드)의 application id 값을 설정합니다. 결제와 통계를 위해 꼭 필요합니다.
 //        BootpayAnalytics.init(this, "5b14c0ffb6d49c40cda92c4e");
 
@@ -34,7 +32,7 @@ public class KakaoPay extends Activity {
 
     public void onClick_request() {
         // 결제호출
-        Bootpay.init(getFragmentManager())
+        Bootpay.init(this)
                 .setApplicationId(application_id) // 해당 프로젝트(안드로이드)의 application id 값
                 .setPG(PG.KCP)
                 .setName("서울식물원 대인 1명") // 결제할 상품명
@@ -52,7 +50,7 @@ public class KakaoPay extends Activity {
                         AppManager.getInstance().getMainActivity().setDateOfPayment();
                         AppManager.getInstance().getPaymentPopUpActivity().finish();
                         AppManager.getInstance().getMenuFloatingActionButton().callOnClick();
-
+                        finish();
                     }
                 })
                 .onReady(new ReadyListener() { // 가상계좌 입금 계좌번호가 발급되면 호출되는 함수입니다.
@@ -65,11 +63,13 @@ public class KakaoPay extends Activity {
                     @Override
                     public void onCancel(@Nullable String message) {
                         finish();
+                        Log.d("cancel", message);
                     }
                 })
                 .onError(new ErrorListener() { // 에러가 났을때 호출되는 부분
                     @Override
                     public void onError(@Nullable String message) {
+                        finish();
                         Log.d("error", message);
                     }
                 })
@@ -77,10 +77,16 @@ public class KakaoPay extends Activity {
                     @Override
                     public void onClose(String message) {
                         finish();
+                        Log.d("close", message);
                     }
                 })
                 .show();
+
     }
 
-
+    @Override
+    public void onBackPressed() {
+        Bootpay.finish();
+        finish();
+    }
 }
