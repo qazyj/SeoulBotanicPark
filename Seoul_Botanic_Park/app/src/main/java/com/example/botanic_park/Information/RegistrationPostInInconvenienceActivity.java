9ea3d276738c77ffa98d5b1b35e33d416bbe4f5c
kimyoungjin12 <qazyj@naver.com>
2019-08-18 14:54:29 +0900

    commit

---
+++ Seoul_Botanic_Park/app/src/main/java/com/example/botanic_park/Information/RegistrationPostInInconvenienceActivity.java
@@ -0,0 +1,16 @@
+package com.example.botanic_park.Information;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.botanic_park.R;
+
+public class RegistrationPostInInconvenienceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_post_in_inconvenience);
+
+
+    }
+}
