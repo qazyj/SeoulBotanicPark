e36b24d9593a7f26835a97b3d06709c890b67fc5
kimyoungjin12 <qazyj@naver.com>
2019-08-19 17:49:24 +0900

    글쓰기 데이터베이스로 넣는 것 완성

--- Seoul_Botanic_Park/app/src/main/java/com/example/botanic_park/Information/InconvenienceActivity.java
+++ Seoul_Botanic_Park/app/src/main/java/com/example/botanic_park/Information/InconvenienceActivity.java
@@ -1,7 +1,11 @@
 package com.example.botanic_park.Information;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.botanic_park.R;
 
 public class InconvenienceActivity extends Activity {
@@ -11,6 +15,13 @@ public class InconvenienceActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inconvenience);
 
+        findViewById(R.id.registration_post_button2).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(InconvenienceActivity.this, RegistrationPostInInconvenienceActivity.class);
+                startActivity(intent);
+            }
+        }
+        );
 
     }
 }
