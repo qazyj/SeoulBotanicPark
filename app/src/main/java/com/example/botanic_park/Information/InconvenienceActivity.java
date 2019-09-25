4fd62110d19c8450cbce2c2ff11287b59acaac23
kimyoungjin12 <qazyj@naver.com>
2019-09-25 17:25:48 +0900

    불편사항 게시판 자동으로 글 순서 숫자 조정

--- app/src/main/java/com/example/botanic_park/Information/InconvenienceActivity.java
+++ app/src/main/java/com/example/botanic_park/Information/InconvenienceActivity.java
@@ -17,7 +17,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -49,6 +51,7 @@ public class InconvenienceActivity extends Activity {
         getWindow().setStatusBarColor(Color.parseColor("#FAFAFA"));
         setContentView(R.layout.activity_inconvenience);
         AppManager.getInstance().setInconvenienceActivity(this);
+
     }
 
     @Override
@@ -58,7 +61,7 @@ public class InconvenienceActivity extends Activity {
             list = (ListView) findViewById(R.id.listviewpost);
             postList = new ArrayList<HashMap<String, String>>();
 
-            getData("http://106.10.37.13/inconvenienceselect.php"); //수정 필요
+            updateData("http://106.10.37.13/inconvenienceselect.php");
 
             findViewById(R.id.registration_post_button2).setOnClickListener(new Button.OnClickListener() {
                                                                                 public void onClick(View v) {
@@ -73,6 +76,127 @@ public class InconvenienceActivity extends Activity {
             Toast.makeText(InconvenienceActivity.this, "네트워크가 연결되지 않아 초기화면으로 돌아갑니다.", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(InconvenienceActivity.this, MainActivity.class);
             startActivity(intent);
+        }
+    }
+
+    public void updateData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                updateList();
+                getData("http://106.10.37.13/inconvenienceselect.php");
+            }
+
+        }
+
+        GetDataJSON g = new GetDataJSON();
+
+        g.execute(url);
+
+    }
+
+    protected void updateList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            posts = jsonObj.getJSONArray(TAG_AMOUNT);
+            for (int i = 0; i <=posts.length()-1; i++) {
+                JSONObject c = posts.getJSONObject(i);
+                String number = c.getString(TAG_NUMBER);
+
+                UpdateData updateTask = new UpdateData();
+                updateTask.execute("http://106.10.37.13/updatepostnumber.php", number, String.valueOf(i+1));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class UpdateData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String number1 = (String)params[1];
+            String number2 = (String)params[2];
+
+            String serverURL = (String)params[0];
+            String postParameters = "number1=" + number1 + "&number2=" + number2;
+
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("texttest", "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d("texttest", "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
         }
     }
 
@@ -99,8 +223,8 @@ public class InconvenienceActivity extends Activity {
             }
             ListAdapter adapter = new SimpleAdapter(
                     InconvenienceActivity.this, postList, R.layout.item_inconvenience_listview,
-            new String[]{TAG_NUMBER, TAG_TITLE, TAG_VIEWS, TAG_REGISTRATION_DATE, TAG_PASSWORD},
-            new int[]{R.id.item_number, R.id.item_title, R.id.item_views,R.id.item_registration_date, R.id.item_password});
+                    new String[]{TAG_NUMBER, TAG_TITLE, TAG_VIEWS, TAG_REGISTRATION_DATE, TAG_PASSWORD},
+                    new int[]{R.id.item_number, R.id.item_title, R.id.item_views,R.id.item_registration_date, R.id.item_password});
 
             list.setOnItemClickListener(new AdapterView.OnItemClickListener()
             {
